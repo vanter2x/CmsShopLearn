477c2417e42e2d30ce97b699ad362705591bd166
vanter2x <vanter2x@gmail.com>
2018-12-27 15:39:25 +0100

    Admin Pages cz.4

--- CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -24,9 +24,57 @@ namespace CmsShop.Areas.Admin.Controllers
         }
 
         //GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //sprawdzanie formularza (ModelState)
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string slug;
+                
+                //inicjalizacja PageDto
+                PageDTO dto = new PageDTO();
+               
+                //gdy nie mamy adresu to przypisujemy tytuł
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                //zapobiegamy dodaniu takiej samej nazwy strony
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("","Ten tytuł lub adres strony już istnieje.");
+                    return View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                //zapis dto do bazy
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+            return RedirectToAction("AddPage");
         }
     }
 }
