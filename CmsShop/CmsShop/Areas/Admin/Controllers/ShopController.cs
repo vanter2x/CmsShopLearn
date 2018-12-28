5ed5ccc1a30212fcbc38c51d1d0cff3e959a11e4
vanter2x <vanter2x@gmail.com>
2018-12-28 20:22:01 +0100

    Admin Shop cz.5

--- CmsShop/CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Web.Mvc;
 using CmsShop.Models.Data;
@@ -22,6 +23,31 @@ namespace CmsShop.Areas.Admin.Controllers
             }
 
             return View(categoryVMList);
+        }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //deklaracja id
+            string id;
+            using (Db db = new Db())
+            {
+                //sprawdzenie czy nazwa kategorii jest unikatowa
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "tytulzajety";
+
+                CategoryDto dto = new CategoryDto();
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 1000;
+
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+            }
+            return id;
         }
     }
 }
